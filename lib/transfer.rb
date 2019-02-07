cab66ae6b0b48103cb36686c48bba93a05285487
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-07 23:35:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,13 @@
 class Transfer
-  attr_reader :status
+  attr_accessor :name, :status, :sender, :reciever, :amount
   
   
-  def initialize(status = "pending")
+  def initialize(name, status = "pending")
+    @name = name 
     @status = status
+    @sender = sender 
+    @reciever = reciever
+    @amount = amount
     
   end
   
