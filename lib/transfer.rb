1e6575f8995148899f52c82809e0cf02bcd4f90e
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-07 23:50:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
   def execute_transaction
     if self.valid?
       @sender.balance = @sender.balance - @amount
-      @receiver.balance= @receiver.balance + @amount
+      @receiver.balance = @receiver.balance + @amount
       @status = "complete"
     else 
       "Transaction rejected. Please check your account balance."
