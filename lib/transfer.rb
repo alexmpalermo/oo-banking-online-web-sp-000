d113c3edfbcaba7f44f5abba757b5d51867e9f7f
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-07 23:54:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   end
   
   def execute_transaction
-    if self.valid?
+    if self.valid? = true 
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
